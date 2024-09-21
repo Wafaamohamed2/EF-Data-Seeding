43de5ad6431c1eb36052eb71b9afafe3a46cf11a
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2024-09-21 20:36:29 +0300

    Add project files.

---
+++ EF-DataSeedng/Migrations/20240921164618_UpdateTables.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EF__Index.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Tags_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Tags",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "BlogId", "Url" },
+                values: new object[] { 1, "www.google.com" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "BlogId", "Content", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "test1", "Post 1" },
+                    { 2, 1, "test2", "Post 2" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
---
+++ EF-DataSeedng/Program.cs
@@ -0,0 +1,26 @@
+﻿using NewEF;
+using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SeedData();
+    }
+
+    public static void SeedData()
+    {
+
+        using (var context = new ApplicationDbContext())
+        {
+            context.Database.EnsureCreated();
+
+            var blog = context.Blogs.FirstOrDefault(b => b.Url == "www.google.com");
+
+            if (blog == null) { context.Blogs.Add(new Blog { Url = " www.google.com" }); }
+
+            context.SaveChanges();
+        }
+
+    }
+}
